25c75f8e430ac0249088ad62335498d019646bde
Udit Bhardwaj <uditbhardwaj692@gmail.com>
2026-07-08 22:15:11 +0530

    Rename hello.py to 01_hello.py

---
+++ 02_variables.py
@@ -0,0 +1,19 @@
+# Variables in Python
+name = "Alice" #String variable
+age = 18  #Integer variable
+height = 5.6  #Float variable
+is_student = True  #Boolean variable
+
+# printing the person's details
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Rules for naming variables in Python
+# 1. Variable names can only contain letters, numbers, and underscores.
+# 2. Variable names can start with a letter or an underscore.
+# 3. Variable names cannot start with a number.
+# 4. Variable names cannot contain spaces or special characters.
+# 5. Python variable names are case-sensitive.
+# Example: age and Age are different variables.
---
+++ 03_data_types.py
@@ -0,0 +1,22 @@
+# Data Types in Python
+
+# String
+name = "Alice"  
+print(type(name))  # <class 'str'>
+
+# Integer
+age = 18  
+print(type(age))  # <class 'int'>
+
+# Float
+height = 5.6  
+print(type(height))  # <class 'float'>
+
+# Boolean
+is_student = True  
+print(type(is_student))  # <class 'bool'>
+
+# NoneType
+e = None  
+print(type(e))  # <class 'NoneType'>
+
