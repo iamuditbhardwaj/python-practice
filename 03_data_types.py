8a34f7ee26e7ae62f669de242e1d1b90570eb2cc
Udit Bhardwaj <uditbhardwaj692@gmail.com>
2026-07-09 11:14:27 +0530

    Learned Data Types

--- 03_data_types.py
+++ 03_data_types.py
@@ -2,21 +2,21 @@
 
 # String
 name = "Alice"  
-print(type(name))  # <class 'str'>
+print(type(name))  # Output: <class 'str'>
 
 # Integer
 age = 18  
-print(type(age))  # <class 'int'>
+print(type(age))  # Output: <class 'int'>
 
 # Float
 height = 5.6  
-print(type(height))  # <class 'float'>
+print(type(height))  # Output: <class 'float'>
 
 # Boolean
 is_student = True  
-print(type(is_student))  # <class 'bool'>
+print(type(is_student))  # Output: <class 'bool'>
 
 # NoneType
 e = None  
-print(type(e))  # <class 'NoneType'>
+print(type(e))  # Output:<class 'NoneType'>
 
