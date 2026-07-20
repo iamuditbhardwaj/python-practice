4ecf92625bb50f60be09eefb1b34c1edd135f744
Udit Bhardwaj <uditbhardwaj692@gmail.com>
2026-07-20 20:19:33 +0530

    Updated strings

--- 09_strings.py
+++ 09_strings.py
@@ -57,6 +57,10 @@ print(slicing[2:5])  # Output: tho
 print(slicing[-6:-3])  # Output: Pyt
 print(slicing[-4:-1])  # Output: tho
 
+# print(slicing[0:3]) and print(slicing[-6:-3]) will give the same output as both are referring to the same characters in the string.
+# Also print(slicing[:3]) and print(slicing[0:3]) will give the same output as the start index is optional and defaults to 0 if not specified.
+# Same goes for the end index, if not specified it defaults to the length of the string.
+
 # Strings can also be sliced with a step value, which allows you to skip characters in the string. 
 # Syntax : variable_name = string[start:end:step]
 # Example:
