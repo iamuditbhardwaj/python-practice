523b844376f571fa0cd4f61b0be5a5ba38dff2dc
Udit Bhardwaj <uditbhardwaj692@gmail.com>
2026-07-22 21:33:35 +0530

    Organized the folders

--- 01_hello.py
+++
@@ -1,7 +0,0 @@
-print("Hello, GitHub!")
-print("My name is Udit.")
-print("I'm starting my Python journey.")
-print("This is my first python code.")
-print("Thank you!")
-
-
--- 04_tuples.py/02_indexing.py
+++
@@ -1,8 +0,0 @@
-# A tuple can be indexed in the same way as strings and lists.
-
-# Tuple Indexing:
-indexing = (84, "b", 33.4, "d", "Henry")
-print(indexing[0])  # Output: 84
-print(indexing[2])  # Output: 33.4
-print(indexing[-1]) # Output: Henry
-# And so on.....
